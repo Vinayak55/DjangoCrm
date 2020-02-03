a4cb25fa40efb5d75c5b14b7dacda289d8a0bbeb
vinayak55 <vinayakrk555@gmail.com>
2020-02-04 01:27:06 +0530

    Crm

--- DjangoCrm/printapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrintappConfig(AppConfig):
-    name = 'printapp'
--- DjangoCrm/printapp/signals.py
+++
@@ -1,3 +0,0 @@
-from django.dispatch import Signal
-
-send_mail1 = Signal(providing_args=["order"])
